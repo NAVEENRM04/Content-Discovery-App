872afd1f1a28ec4063f731e9638249422bf35263
NAVEENRM04 <727821tucs136@skct.edu.in>
2023-10-19 01:37:53 +0530

    aws push

---
+++ aws/newprj_n/src/main/java/com/example/demo/Controller/imageController.java
@@ -0,0 +1,86 @@
+package com.example.demo.Controller;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.ImageUserEntity;
+import com.example.demo.Entity.User;
+import com.example.demo.Entity.imageEntity;
+import com.example.demo.Service.UserService;
+import com.example.demo.Service.imageService;
+import com.example.demo.repo.UserRepoInt;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/permitall")
+public class imageController {
+	@Autowired
+	private imageService is;
+	@Autowired
+	private UserService ss;
+	@Autowired
+	private UserRepoInt sri;
+	
+	@GetMapping("/image/{category}")
+	public List<imageEntity>getImageByCategory(@PathVariable String category)
+	{
+		return is.findByCategory(category);
+	}
+	
+	@PostMapping("/image/savedata")
+	public String savedata(@RequestBody imageEntity ie)
+	{	
+		is.postData(ie);
+		return "image added";
+	}
+	@GetMapping("/image/data")
+	public List<imageEntity>getImageData()
+	{
+		return is.getAllImage();
+	}
+	@GetMapping("/{username}/getAll")
+	public ResponseEntity<List<imageEntity>> getAllImages(@PathVariable String username) {
+		
+		return ResponseEntity.ok(is.getAllImages(username));
+	}
+	
+	@PutMapping("/updateimage/likes/{imageid}/{username}")
+	public String updatelike(@PathVariable int imageid,@PathVariable String username)
+	{
+		imageEntity image = is.findByImageId(imageid);
+		User user = ss.findByUsername(username);
+		if(user.getLikes()==null) {
+			Set<Integer> likes = new HashSet<>();
+			user.setLikes(likes);
+		}
+		if(user.getLikes().add(imageid))
+		{
+			image.setLikes(image.getLikes()+1);
+			is.updateLikeData(image);
+			sri.save(user);
+			return "likes added";
+		}
+		else 
+		{
+			image.setLikes(image.getLikes()-1);
+			is.updateLikeData(image);
+			user.getLikes().remove(imageid);
+			sri.save(user);
+			return "likes removed";
+		}
+	}
+	
+	
+	
+
+}
